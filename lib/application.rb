130cd8439bf99a51e5fc9b8fbc49990e1526dca1
Jeremy Olliver <jeremy.olliver@gmail.com>
2009-06-21 17:30:36 +1200

    Formatted most view elements. Still trying to figure out popup button and checkbox

--- lib/application.rb
+++ lib/application.rb
@@ -9,9 +9,35 @@ class Application
   def start
     application :name => "Qif2csv" do |app|
       app.delegate = self
-      window :frame => [100, 100, 500, 500], :title => "Qif2csv" do |win|
-        win << label(:text => "Hello from HotCocoa", :layout => {:start => false})
+      window :size => [500, 500], :center => true, :title => "Qif2csv", :view => :nolayout do |win|
         win.will_close { exit }
+        
+        # Window elements layout
+        win.view = layout_view(:layout => {:expand => [:width, :height], :padding => 0, :margin => 0, :start => false}) do |vert|
+          # Title
+          vert << label(:text => "Qif2csv - .qif file conversion tool", :layout => {:align => :center, :start => false})
+          # File display
+          vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal, :layout => {:padding => 0, :margin => 0, :start => false, :expand => [:width]}) do |horiz|
+            horiz << @file_label = label(:text => "File: ", :layout => {:expand => [:width]})
+            horiz << @file_open_button = button(:title => "Open", :layout => {:align => :right}) do |b|
+              b.on_action { load_file }
+            end
+          end
+          # Formatting options
+          vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal, :layout => {:padding => 0, :margin => 0, :start => false, :expand => [:width]}) do |horiz|
+            # FIXME: turn into a PopupButton
+            horiz << @file_label = label(:text => "File: ", :layout => {:align => :left, :expand => [:width]})
+            # FIXME: turn into a checkbox ()
+            horiz << @windows_formatted = button(:title => "Format for windows", :layout => {:align => :left})
+          end
+          # Output dialog and button
+          vert << layout_view(:frame => [0, 0, 0, 40], :mode => :horizontal, :layout => {:padding => 0, :margin => 0, :start => false, :expand => [:width]}) do |horiz|
+            horiz << @feed_field = text_field(:layout => {:align => :left, :expand => [:width]})
+            horiz << @convert_button = button(:title => "Convert", :layout => {:align => :right}) do |b|
+              b.on_action { convert }
+            end
+          end
+        end
       end
     end
   end
@@ -39,6 +65,16 @@ class Application
   # window/bring_all_to_front
   def on_bring_all_to_front(menu)
   end
+  
+  # When the open button is clicked
+  def load_file
+    
+  end
+  
+  def convert
+    # TODO: implementation
+  end
+  
 end
 
 Application.new.start
